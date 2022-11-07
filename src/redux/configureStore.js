eb175fcd9ee5c281ff3c7a329ac2735ace29ab94
Zuhaib Amjad <umar0423@gmail.com>
2022-11-07 22:25:46 +0500

    Implement redux functionality in configureStore.js file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,2 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from '@reduxjs/toolkit';
+import addremoveReducer from './books/books';
+import checkstatusReducer from './categories/categories';
+
+const rootReducer = combineReducers({
+  reducer: {
+    addremoveReducer,
+    checkstatusReducer,
+  },
+});
+
+const store = configureStore(rootReducer);
+
+export default store;
