f1352f2402b624a829a7bcdd9173c50c8c6dca25
Zuhaib Amjad <umar0423@gmail.com>
2022-11-08 19:50:46 +0500

    Implement checkStatus functionality
    using useDispatch and useSelector hooks from redux

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,23 +1,11 @@
 import React from 'react';
-// import { v4 as uuidv4 } from 'uuid';
 import { useSelector } from 'react-redux';
 import AddBook from './AddBook';
 import BookItem from './BookItem';
 
 const BooksList = () => {
-  // const [books, setBooks] = useState([]);
   const books = useSelector((state) => state.addremove);
-  // console.log(books);
 
-  // const addABook = (title, author, category) => {
-  //   const book = {
-  //     id: uuidv4(),
-  //     title,
-  //     author,
-  //     category,
-  //   };
-  //   setBooks([...books, book]);
-  // };
   const booksElements = books.map((book) => (
     <BookItem
       title={book.title}
--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div>
-    <div className="flex justify-center items-center">
-      <button
-        className="mx-auto mt-12 px-2 rounded bg-slate-200 text-zinc-500"
-        type="button"
-      >
-        Check Status
-      </button>
+const Categories = () => {
+  const status = useSelector((state) => state.checkstatus);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div className="flex w-full flex-col justify-center items-center">
+        <button
+          className="mx-auto mt-12 px-2 rounded bg-slate-200 text-zinc-500"
+          type="button"
+          onClick={() => dispatch(checkStatus(status))}
+        >
+          Check Status
+        </button>
+        <p className="text-black">{status}</p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Categories;
