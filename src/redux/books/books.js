d0719216154d339eacf367b7fec9b3dc386e8a6a
Zuhaib Amjad <umar0423@gmail.com>
2022-11-10 19:46:00 +0500

    Fetch data from api and render it on display successfully

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,32 +1,34 @@
 // Actions
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const GET_BOOKS = 'bookstore/books/GET_BOOKS';
+
+const baseUrl = // eslint-disable-line
+  'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/gINbrGI6GybOSE438a8P/books';
+
+const fetchBooksfromApi = async () => {
+  const res = await fetch(baseUrl, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const allBooks = await res.json();
+  const books = Object.keys(allBooks).map((id) => ({
+    id,
+    title: allBooks[id][0].title,
+    author: allBooks[id][0].author,
+    category: allBooks[id][0].category,
+  }));
+  // .sort((a, b) => a.title.localeCompare(b.title));
+  return books;
+};
 
 // state data
-
-const data = [
-  {
-    id: 1,
-    title: 'Prince of Persia',
-    author: 'K.L Ceasar',
-    category: 'History',
-  },
-  {
-    id: 2,
-    title: 'Optimus Prime',
-    author: 'Luca King',
-    category: 'Sci-fi',
-  },
-  {
-    id: 3,
-    title: 'Rings of Power',
-    author: 'Tolkien',
-    category: 'Fantasy',
-  },
-];
+const initialState = [];
 
 // Reducer
-const addremoveReducer = (state = data, action = {}) => {
+const addremoveReducer = (state = initialState, action = {}) => {
   switch (action.type) {
     // do reducer stuff
     case ADD_BOOK: {
@@ -35,12 +37,24 @@ const addremoveReducer = (state = data, action = {}) => {
     case REMOVE_BOOK: {
       return [...state.filter((book) => book.id !== action.payload)];
     }
+    case GET_BOOKS: {
+      return action.payload;
+    }
     default:
       return state;
   }
 };
 
 // Action Creators
+
+export const fetchBooks = () => async (dispatch) => {
+  const payload = await fetchBooksfromApi();
+  dispatch({
+    type: GET_BOOKS,
+    payload,
+  });
+};
+
 export const addBook = (payload) => ({
   type: ADD_BOOK,
   payload,
